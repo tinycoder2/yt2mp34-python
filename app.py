ac8eef68f2b2e7a96085661367a7899ba1a9c1e5
tinycoder2 <swaathykumar02@gmail.com>
2022-01-17 19:49:07 +0530

    basic functionality : mp4 conversion

--- app.py
+++ app.py
@@ -1,9 +1,30 @@
 from flask import Flask, render_template, request,redirect, url_for
 from pytube import YouTube
 from pathlib import Path
+from flask_bootstrap import Bootstrap
+from flask_nav import Nav
+from flask_nav.elements import *
+from dominate.tags import img
+
+
+
+logo = img(src='./static/img/forg.png', height="50", width="50", style="margin-top:-15px")
+
+topbar = Navbar(logo,
+                View('Home', 'home'),
+                View('About Me', 'aboutMe'),
+                )
+
+# registers the "top" menubar
+nav = Nav()
+nav.register_element('top', topbar)
 
 app = Flask(__name__)
+Bootstrap(app)
 
+@app.route('/aboutMe')
+def aboutMe():
+    return redirect("https://tinycoder2.github.io/pimbuOS/")
 
 def ytConv(url):
     downloadFolder = str(Path.home() / "Downloads")
@@ -15,9 +36,9 @@ def ytConv(url):
     stream.download(downloadFolder)
     print("downloaded")
 
-@app.route('/form')
-def form():
-   return render_template("get_url.html")
+# @app.route('/form')
+# def form():
+#    return render_template("get_url.html")
 
 
 @app.route('/get_Input', methods = ['GET','POST'])
@@ -36,7 +57,9 @@ def getInput():
 
 @app.route("/")
 def home():
-    return "Hello, Flask!"
+    return render_template("index.html")
 
+
+nav.init_app(app)
 if __name__ == '__main__':
    app.run()
